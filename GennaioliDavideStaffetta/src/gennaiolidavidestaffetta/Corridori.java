88fc512741447fe4774ff28b959ec24d808c9439
Davide Gennnaioli <gennaioli.davide@franchettisalviani.net>
2026-04-08 11:43:18 +0200

    aggiunto progetto Staffetta

---
+++ GennaioliDavideStaffetta/src/gennaiolidavidestaffetta/Corridori.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gennaiolidavidestaffetta;
+
+/**
+ *
+ * @author gennaioli.davide
+ */
+public class Corridori implements Runnable {
+     private int percentuale;
+     
+     public Corridori( int percentuale){
+         this.percentuale=percentuale;
+     }
+     
+     @Override
+     public void run()
+     {
+         
+     }
+}
